3d610c9f1f4160d832db9143c92568a556e78936
cuongtk1002 <tkcuong1002@gmail.com>
2023-02-07 22:28:55 +0700

    firts commit

---
+++ src/com/codegym/findmaxvalue/FindMaxValue.java
@@ -0,0 +1,23 @@
+package com.codegym.findmaxvalue;
+
+import java.util.List;
+
+public class FindMaxValue {
+    public static String fileDirPath = "/Users/admin/Desktop/Module two/lesson-16/DemoTextFile/src/com/codegym";
+    public static int findMax(List<Integer> numbers) {
+        int max = numbers.get(0);
+        for (int i = 0; i < numbers.size(); i++) {
+            if (max < numbers.get(i)) {
+                max = numbers.get(i);
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        ReadAndWriteFile readAndWriteFile = new ReadAndWriteFile();
+        List<Integer> numbers = readAndWriteFile.readFile(fileDirPath + "/numbers.txt");
+        int maxValue = findMax(numbers);
+        readAndWriteFile.writeFile(fileDirPath + "/result.txt", maxValue);
+    }
+}
